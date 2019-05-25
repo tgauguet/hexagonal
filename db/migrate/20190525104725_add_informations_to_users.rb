053d589aa937bb1259a3f6147531c3aa4ce588a5
Tim <timothee.gauguet@orange.fr>
2019-05-25 12:48:03 +0200

    update : add new columns to user

---
+++ db/migrate/20190525104725_add_informations_to_users.rb
@@ -0,0 +1,7 @@
+class AddInformationsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :bio, :text
+  end
+end
