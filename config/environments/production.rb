89666b9e0fa068c3b2c4a85fa6166f50309488ab
Tim <timothee.gauguet@orange.fr>
2019-05-28 11:45:17 +0200

    fix : add error delivery to email production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'hexagoworking@gmail.com"',
+    user_name:            'hexagoworking@gmail.com',
     password:             'myjbaF-zedwu2-kywzub',
     authentication:       'plain'
     # enable_starttls_auto: true
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
